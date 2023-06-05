a78bf1d89ffda2f6fade6f669459ce672a91fc63
rashedrk <rashedrk001@gmail.com>
2023-06-05 22:57:36 +0600

    search option removed and final

--- src/Pages/AllTasks/AllTasks.jsx
+++ src/Pages/AllTasks/AllTasks.jsx
@@ -76,7 +76,7 @@ const AllTasks = () => {
     }
     return (
         <div>
-            {/* search option  */}
+            {/* search option 
             <div className="form-control mb-5">
                 <div className="input-group justify-center">
                     <input type="text" placeholder="Search by Name or Status" className="input input-bordered" />
@@ -84,7 +84,7 @@ const AllTasks = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
                 </div>
-            </div>
+            </div> */}
             {/* table to show all task  */}
             <table className="table w-full">
                 {/* head */}
